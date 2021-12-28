a3e57f9f24025780dbbd28d20db989a3dc516a19
todd nielsen <tgnielsen1145@gmail.com>
2021-12-28 11:46:34 -0700

    fdsf

--- chat_app/lib/views/logout/logout_screen.dart
+++ chat_app/lib/views/logout/logout_screen.dart
@@ -3,50 +3,39 @@ import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:chat_app/views/drawer/menu_drawer.dart';
 
 class LogoutScreen extends StatefulWidget {
-  static const routeName='/logout-screen';
-  const LogoutScreen({ Key? key }) : super(key: key);
+  static const routeName = '/logout-screen';
+  const LogoutScreen({Key? key}) : super(key: key);
 
   @override
   _LogoutScreenState createState() => _LogoutScreenState();
 }
 
 class _LogoutScreenState extends State<LogoutScreen> {
-  final _auth = auth.FirebaseAuth.instance;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Logout')
-      ),
-      drawer: MenuDrawer(),
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10)
-              ),
-              ElevatedButton(
-                child: Text('Logout'),
-                onPressed: (){
-                   Navigator.of(context).pop(); //close the drawer
-            ///the below function simply calls the home route, in other words
-            ///by just passing / into pushReplacementNamed it will navigate back
-            ///to the main.dart file this insures that the consumer will run
-            ///run again anytime we press logout
-            Navigator.of(context).pushReplacementNamed('/');
-           final _auth =auth.FirebaseAuth.instance;
-           _auth.signOut();
-
-                },
+        appBar: AppBar(title: Text('Logout')),
+        drawer: MenuDrawer(),
+        body: SingleChildScrollView(
+            child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(16.0),
+                child: Column(children: <Widget>[
+                  Padding(padding: const EdgeInsets.all(10)),
+                  ElevatedButton(
+                    child: Text('Logout'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacementNamed('/');
+                      final _auth = auth.FirebaseAuth.instance;
+                      _auth.signOut();
+                    },
+                  )
+                ]
               )
-            ]
+            )
           )
-        )
-      )
-    );
+        );
   }
 }
--- chat_app/lib/views/password/reset_password_screen.dart
+++ chat_app/lib/views/password/reset_password_screen.dart
@@ -6,8 +6,8 @@ import 'package:chat_app/views/drawer/menu_drawer.dart';
 import 'package:chat_app/models/user.dart';
 
 class ResetPasswordScreen extends StatefulWidget {
-  static const routeName='/reset-password-screen';
-  const ResetPasswordScreen({ Key? key }) : super(key: key);
+  static const routeName = '/reset-password-screen';
+  const ResetPasswordScreen({Key? key}) : super(key: key);
 
   @override
   _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
@@ -18,8 +18,8 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   final _form = GlobalKey<FormState>();
   final _auth = auth.FirebaseAuth.instance;
 
-  resetPassword(){
-     String? _id = auth.FirebaseAuth.instance.currentUser!.uid;
+  resetPassword() {
+    String? _id = auth.FirebaseAuth.instance.currentUser!.uid;
     return FutureBuilder<DocumentSnapshot>(
         // future:  FirebaseUtil.getCurrentUser(_id),
         future: usersReference.doc(_id).get(),
@@ -28,58 +28,47 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
             return CircularProgressIndicator();
           }
           user = User.fromDocument(snapshot.data!);
-         // _image = File(user!.profilePic!);
-    _auth.sendPasswordResetEmail(email: user!.email!);
-    return SingleChildScrollView(
-      child:Container(
-       margin: EdgeInsets.only(left: 16.0, right: 16, bottom: 16),
-
-      )
-    );
+          // _image = File(user!.profilePic!);
+          _auth.sendPasswordResetEmail(email: user!.email!);
+          return SingleChildScrollView(
+              child: Container(
+            margin: EdgeInsets.only(left: 16.0, right: 16, bottom: 16),
+          ));
         });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Reset Password'),
-        actions: <Widget>[
+        appBar: AppBar(title: Text('Reset Password'), actions: <Widget>[
           // IconButton(
           //   icon: Icon(Icons.save),
           //   onPressed: (){},
           // )
-        ]
-      ),
-      drawer: MenuDrawer(),
-      body:SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(16.0),
-          child: Form(
-            key: _form,
-            child:Column(
-              children:<Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(10)
-                  ),
-                  resetPassword(),
-                  Container(
-                    alignment: Alignment.center,
-           
-               child: Text('Reset password email has been sent!',
-               
-               style: TextStyle(
-                 fontFamily: 'Lato',
-                 fontSize: 30,
-                 color: Colors.black,                 
-               )),
-               
-              ),
-                  
-              ]
+        ]),
+        drawer: MenuDrawer(),
+        body: SingleChildScrollView(
+            child: Container(
+                margin: EdgeInsets.all(16.0),
+                child: Form(
+                    key: _form,
+                    child: Column(children: <Widget>[
+                      Padding(padding: const EdgeInsets.all(10)),
+                      resetPassword(),
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text('Reset password email has been sent!',
+                            style: TextStyle(
+                              fontFamily: 'Lato',
+                              fontSize: 30,
+                              color: Colors.black,
+                            )),
+                      ),
+                    ]
+                  )
+                )
+              )
             )
-          )
-        )
-      )
-    );
+          );
   }
 }
